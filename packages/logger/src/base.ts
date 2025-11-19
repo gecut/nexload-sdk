1dc9d549b077df1d5595c14f003b13627a97f757
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2025-11-19 14:07:07 +0330

    chore: update dependencies and improve logger configuration
    - Added `thread-stream` dependency to the logger package.
    - Updated `@orpc/client` and `@orpc/contract` dependencies to version 1.11.3.
    - Updated `@types/react` to version 19.2.6 across multiple packages.
    - Enhanced logger configuration to conditionally use `pino-pretty` based on the environment.
    - Updated ESLint and TypeScript dependencies to their latest versions for improved linting and type-checking.

--- packages/logger/src/base.ts
+++ packages/logger/src/base.ts
@@ -1,34 +1,36 @@
 import { EnvManager, merge } from "@nexload-sdk/env";
 import { $NodePreset } from "@nexload-sdk/env/presets";
 import pino from "pino";
-import pretty from "pino-pretty";
 
 const env = new EnvManager("logger", merge($NodePreset));
-const stream = pretty({
-  colorize: pretty.isColorSupported,
-  ignore: "pid,hostname",
-});
 export const serviceName = env.$("SERVICE_NAME");
-export const logger = pino(
-  {
-    name: serviceName,
-    level: env.$("LOG_LEVEL"),
-    timestamp: pino.stdTimeFunctions.isoTime,
-    formatters: {
-      level(label) {
-        return { level: label.toUpperCase() };
-      },
-    },
-    base: { pid: process.pid },
-    redact: {
-      paths: [
-        "req.headers.authorization",
-        "req.headers.cookie",
-        "user.password",
-        "user.secret",
-      ],
-      censor: "[REDACTED]",
+export const logger = pino({
+  name: serviceName,
+  level: env.$("LOG_LEVEL"),
+  transport:
+    env.$("NODE_ENV") !== "production"
+      ? {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            ignore: "pid,hostname",
+          },
+        }
+      : undefined,
+  timestamp: pino.stdTimeFunctions.isoTime,
+  formatters: {
+    level(label) {
+      return { level: label.toUpperCase() };
     },
   },
-  stream
-);
+  base: { pid: process.pid },
+  redact: {
+    paths: [
+      "req.headers.authorization",
+      "req.headers.cookie",
+      "user.password",
+      "user.secret",
+    ],
+    censor: "[REDACTED]",
+  },
+});
