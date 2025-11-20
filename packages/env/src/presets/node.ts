87a7ac0ebd771ca2dbe8ab15c209a6e19087f4e1
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2025-11-20 16:07:46 +0330

    fix(node preset): change default LOG_LEVEL from 'info' to 'trace'

--- packages/env/src/presets/node.ts
+++ packages/env/src/presets/node.ts
@@ -2,6 +2,6 @@ import { EnvPreset } from "../manager/types";
 
 export const $NodePreset = {
   NODE_ENV: { type: "string", default: "development", },
-  LOG_LEVEL: { type: "string", default: "info", },
+  LOG_LEVEL: { type: "string", default: "trace", },
   SERVICE_NAME: { type: "string", default: "UNKNOWN", },
 } as const satisfies EnvPreset;
